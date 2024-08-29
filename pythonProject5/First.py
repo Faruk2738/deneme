db28efa39a8ee12068ecccf9d3144f41ce46c333
Faruk <oftnl2738@gmail.com>
2024-08-29 16:35:30 +0200

    Hello

--- pythonProject5/First.py
+++ pythonProject5/First.py
@@ -1,2 +1,40 @@
 #
 # Merhaba arkadaslar
+
+##  Veri degisimleri ##
+
+#  integer to string
+
+x = 42
+mytext = str(x)
+print(mytext)
+
+
+# string to float
+
+tekst = "15.22"
+myfloat = float(tekst)
+print(myfloat)
+
+
+# list to tuple
+
+liste = [1, 2, 3]
+mytuple = tuple(liste)
+print(mytuple)
+type(mytuple)
+
+
+# Tuple to list
+
+my_tuple = (4, 5, 6)
+listem = list(my_tuple)
+print(listem)
+type(listem)
+
+# list to set
+
+liste = [1, 2, 3, 4]
+my_set  = set(liste)
+print(my_set)
+type(my_set)
