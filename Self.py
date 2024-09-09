44e562c7b0c02a77adf2201782a4abe78c32280b
Faruk <oftnl2738@gmail.com>
2024-09-09 18:08:27 +0200

    Fist.py, Self.py, Task.py added

--- Self.py
+++ Self.py
@@ -450,8 +450,8 @@ def calculate(varm, moisture, charge):
 # calculate(98, 12, 78) * 10
 
 
-# def calculate(varm, moisture, charge):
-      return (varm + moisture) / charge
+def calculate(varm, moisture, charge):
+    return (varm + moisture) / charge
 
 
 # calculate(98, 12, 78) * 10
@@ -472,4 +472,496 @@ type(calculate(98, 12, 78))
 
 
 ## Fonksiyon icerisinden fonksiyon cagirmak
+
+
+def calculate(varm, moisure, charge):
+    return int(varm + moisure) / charge
+
+calculate(90, 12, 12) * 10
+
+
+
+## Kosullar (Conditions)
+
+# True- False hatirlayalim
+
+1 == 1
+1 == 2
+
+if 1 == 1:
+    print("something")
+
+
+number = 11
+if number == 10:
+    print("number is 10")
+
+
+def number_check(number):
+    if number == 10:
+        print("number is 10")
+
+number_check(12)
+
+
+def number_check(number):
+    if number == 10:
+        print("number is 10")
+    else:
+        print("number is not 10")
+
+
+number_check(12)
+
+
+## DONGULER
+
+## for loop
+
+students = ["John", "Mark", "Venessa", "Mariam"]
+
+students[0]
+students[1]
+students[2]
+
+
+for student in students:
+    print(student)
+
+
+for student in students:
+    print(student.upper())
+
+
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+for salary in salaries:
+    print(salary)
+
+
+for salary in salaries:
+    #print(salary*20/100 + salary)
+    print(int(salary * 50 / 100 + salary))
+
+
+def new_salary(salary, rate):
+    return int(salary*rate/100 + salary)
+
+new_salary(1500, 10)
+new_salary(2000, 20)
+
+
+salaries2 = [10700, 25000, 30400, 40300, 50200]
+
+for salary in salaries2:
+    print(new_salary(salary, 15))
+
+for salary in salaries:
+    if salary >= 3000:
+        print(new_salary(salary, 10))
+    else:
+        print(new_salary(salary, 20))
+print(salaries)
+
+
+#######################
+# Uygulama - Mülakat Sorusu
+#######################
+
+# Amaç: Aşağıdaki şekilde string değiştiren fonksiyon yazmak istiyoruz.
+
+# before: "hi my name is john and i am learning python"
+# after: "Hi mY NaMe iS JoHn aNd i aM LeArNiNg pYtHoN"
+
+range(len("miuul"))
+range(0, 5)
+
+# 4 % 2 == 0
+
+# for i in range(0, 5):
+    print(i)
+
+def alternating(string):
+    new_string = ""
+    for string_index in range(len(string)):
+        if string_index % 2 == 0:
+            new_string += string[string_index].upper()
+        else:
+            new_string += string[string_index].lower()
+    print(new_string)
+
+        print (string_index)
+alternating("miuul")
+
+
+#######################
+# break & continue & while
+#######################
+
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+for salary in salaries:
+    if salary == 3000:
+        break
+    print(salary)
+
+
+for salary in salaries:
+    if salary == 3000:
+        continue
+    print(salary)
+
+
+# while
+
+number = 1
+while number < 5:
+    print(number)
+    number += 1
+
+#######################
+# Enumerate: Otomatik Counter/Indexer ile for loop
+#######################
+
+# students = ["John", "Mark", "Venessa", "Mariam"]
+
+# for student in students:
+    print(student)
+
+# for index, student in enumerate(students):
+    print(index, student)
+
+
+students = ["John", "Mark", "Venessa", "Mariam"]
+
+A = []
+B = []
+
+for index, student in enumerate(students):
+    if index % 2 == 0:
+        A.append(student)
+    else:
+        B.append(student)
+
+
+#######################
+# Uygulama - Mülakat Sorusu
+#######################
+# divide_students fonksiyonu yazınız.
+# Çift indexte yer alan öğrencileri bir listeye alınız.
+# Tek indexte yer alan öğrencileri başka bir listeye alınız.
+# Fakat bu iki liste tek bir liste olarak return olsun.
+
+students = ["John", "Mark", "Venessa", "Mariam"]
+
+def divide_students(students):
+    groups = [[], []]
+    for index, student in enumerate(students):
+        if index % 2 == 0:
+            groups[0].append(student)
+        else:
+            groups[1].append(student)
+    print(groups)
+    return groups
+divide_students(students)
+
+st = divide_students(students)
+st[0]
+st[1]
+
+
+#######################
+# alternating fonksiyonunun enumerate ile yazılması
+#######################
+
+def alternating_with_enumerate(string):
+    new_string = ""
+    for i, letter in enumerate(string):
+        if i % 2 == 0:
+            new_string += letter.upper()
+        else:
+            new_string += letter.lower()
+    print(new_string)
+
+alternating_with_enumerate("hi my name is john and i am learning python")
+
+#######################
+# Zip
+#######################
+
+students = ["John", "Mark", "Venessa", "Mariam"]
+
+departments = ["mathematics", "statistics", "physics", "astronomy"]
+
+ages = [23, 30, 26, 22]
+
+list(zip(students, departments, ages))
+
+###############################################
+# lambda, map, filter, reduce
+###############################################
+
+def summer(a, b):
+    return a + b
+
+summer(1, 3) * 9
+
+new_sum = lambda a, b: a + b
+
+new_sum(4, 5)
+
+# map
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+def new_salary(x):
+    return x * 20 / 100 + x
+
+
+new_salary(5000)
+
+for salary in salaries:
+    print(new_salary(salary))
+
+list(map(new_salary, salaries))
+
+
+# del new_sum
+list(map(lambda x: x * 20 / 100 + x, salaries))
+list(map(lambda x: x ** 2 , salaries))
+
+# FILTER
+list_store = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list(filter(lambda x: x % 2 == 0, list_store))
+
+# REDUCE
+from functools import reduce
+list_store = [1, 2, 3, 4]
+reduce(lambda a, b: a + b, list_store)
+
+###############################################
+# COMPREHENSIONS
+###############################################
+
+#######################
+# List Comprehension
+#######################
+
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+def new_salary(x):
+    return x * 20 / 100 + x
+
+for salary in salaries:
+    print(new_salary(salary))
+
+
+null_list = []
+
+for salary in salaries:
+    null_list.append(new_salary(salary))
+
+null_list = []
+
+for salary in salaries:
+    if salary > 3000:
+        null_list.append(new_salary(salary))
+    else:
+        null_list.append(new_salary(salary * 2))
+
+[new_salary(salary * 2) if salary < 3000 else new_salary(salary) for salary in salaries]
+[salary * 2 for salary in salaries]
+[salary * 2 for salary in salaries if salary < 3000]
+[salary * 2 if salary < 3000 else salary * 0 for salary in salaries]
+[new_salary(salary * 2) if salary < 3000 else new_salary(salary * 0.2) for salary in salaries]
+
+####
+students = ["John", "Mark", "Venessa", "Mariam"]
+students_no = ["John", "Venessa"]
+
+
+[student.lower() if student in students_no else student.upper() for student in students]
+[student.upper() if student not in students_no else student.lower() for student in students]
+
+
+#######################
+# Dict Comprehension
+#######################
+
+dictionary = {'a': 1,
+              'b': 2,
+              'c': 3,
+              'd': 4}
+
+dictionary.keys()
+dictionary.values()
+dictionary.items()
+
+{k: v ** 2 for (k, v) in dictionary.items()}
+{k.upper(): v for (k, v) in dictionary.items()}
+{k.upper(): v * 2 for (k, v) in dictionary.items()}
+
+
+#######################
+# Uygulama - Mülakat Sorusu
+#######################
+
+# Amaç: çift sayıların karesi alınarak bir sözlüğe eklenmek istemektedir
+# Key'ler orjinal değerler value'lar ise değiştirilmiş değerler olacak.
+
+
+numbers = range(10)
+new_dict = {}
+
+for n in numbers:
+    if n % 2 == 0:
+        new_dict[n] = n ** 2
+
+{n: n ** 2 for in numbers if n % 2 == 0}
+
+
+
+#######################
+# List & Dict Comprehension Uygulamalar
+#######################
+
+#######################
+# Bir Veri Setindeki Değişken İsimlerini Değiştirmek
+#######################
+
+# before:
+# ['total', 'speeding', 'alcohol', 'not_distracted', 'no_previous', 'ins_premium', 'ins_losses', 'abbrev']
+
+# after:
+# ['TOTAL', 'SPEEDING', 'ALCOHOL', 'NOT_DISTRACTED', 'NO_PREVIOUS', 'INS_PREMIUM', 'INS_LOSSES', 'ABBREV']
+
+
+import seaborn as sns
+df = sns.load_dataset("car_crashes")
+df.columns
+
+for col in df.columns:
+    print(col.lower())
+
+
+A = []
+
+for col in df.columns:
+    A.append(col.upper())
+
+df.columns = A
+
+df = sns.load_dataset("car_crashes")
+
+df.columns = [col.upper() for col in df.columns]
+
+
+#######################
+# İsminde "INS" olan değişkenlerin başına FLAG diğerlerine NO_FLAG eklemek istiyoruz.
+#######################
+
+# before:
+# ['TOTAL',
+# 'SPEEDING',
+# 'ALCOHOL',
+# 'NOT_DISTRACTED',
+# 'NO_PREVIOUS',
+# 'INS_PREMIUM',
+# 'INS_LOSSES',
+# 'ABBREV']
+
+# after:
+# ['NO_FLAG_TOTAL',
+#  'NO_FLAG_SPEEDING',
+#  'NO_FLAG_ALCOHOL',
+#  'NO_FLAG_NOT_DISTRACTED',
+#  'NO_FLAG_NO_PREVIOUS',
+#  'FLAG_INS_PREMIUM',
+#  'FLAG_INS_LOSSES',
+#  'NO_FLAG_ABBREV']
+
+[col for col in df.columns if "INS" in col]
+
+["FLAG_" + col for col in df.columns if "INS" in col]
+
+["FLAG_" + col if "INS" in col else "NO_FLAG_" + col for col in df.columns]
+
+df.columns = ["FLAG_" + col if "INS" in col else "NO_FLAG_" + col for col in df.columns]
+
+
+
+#######################
+# Amaç key'i string, value'su aşağıdaki gibi bir liste olan sözlük oluşturmak.
+# Sadece sayısal değişkenler için yapmak istiyoruz.
+#######################
+
+# Output:
+# {'total': ['mean', 'min', 'max', 'var'],
+#  'speeding': ['mean', 'min', 'max', 'var'],
+#  'alcohol': ['mean', 'min', 'max', 'var'],
+#  'not_distracted': ['mean', 'min', 'max', 'var'],
+#  'no_previous': ['mean', 'min', 'max', 'var'],
+#  'ins_premium': ['mean', 'min', 'max', 'var'],
+#  'ins_losses': ['mean', 'min', 'max', 'var']}
+
+import seaborn as sns
+df = sns.load_dataset("car_crashes")
+df.columns
+
+num_cols = [col for col in df.columns if df[col].dtype != "O"]
+soz = {}
+agg_list = ["mean", "min", "max", "sum"]
+
+for col in num_cols:
+    soz[col] = agg_list
+
+## kisa yol
+
+{col: agg_list for col in num_cols}
+
+new_dict = {col: agg_list for col in num_cols}
+
+df[num_cols].head()
+df[num_cols].agg(new_dict)
+
+
+
+###############################################
+# PYTHON İLE VERİ ANALİZİ (DATA ANALYSIS WITH PYTHON)
+###############################################
+# - NumPy
+# - Pandas
+# - Veri Görselleştirme: Matplotlib & Seaborn
+# - Gelişmiş Fonksiyonel Keşifçi Veri Analizi (Advanced Functional Exploratory Data Analysis)
+
+#############################################
+# NUMPY
+#############################################
+
+# Neden NumPy? (Why Numpy?)
+# NumPy Array'i Oluşturmak (Creating Numpy Arrays)
+# NumPy Array Özellikleri (Attibutes of Numpy Arrays)
+# Yeniden Şekillendirme (Reshaping)
+# Index Seçimi (Index Selection)
+# Slicing
+# Fancy Index
+# Numpy'da Koşullu İşlemler (Conditions on Numpy)
+# Matematiksel İşlemler (Mathematical Operations)
+
+#############################################
+# Neden NumPy?
+#############################################
+
+import numpy as np
+a = [1, 2, 3, 4]
+b = [2, 3, 4, 5]
+
+ab = []
+
+for i in range(0, len(a)):
+    ab.append(a[i] * b[i])
+
+
+a = np.array([1, 2, 3, 4])
+b = np.array([2, 3, 4, 5])
+a * b
 
--- Task.py
+++ Task.py
@@ -6,12 +6,13 @@
 
 ## Görev 1: Kendi isminizde bir virtual environment oluşturunuz, oluşturma esnasında python 3 kurulumu yapınız.
 
-## Cevap: conda create –n env faruk
-##        conda install python 3
+## Cevap: conda create –n farukenv python=3
+#         conda install python 3
+##        conda env list (tüm env görebilirsiniz)
 
 
 ## Görev 2: Oluşturduğunuz environment'ı aktife diniz.
-## Cevap:   conda activate env faruk
+## Cevap:   conda activate farukenv
 
 
 ## Görev 3: Yüklü paketleri listeleyiniz.
@@ -46,4 +47,361 @@
 ## Görev 10: Oluşturduğunuz environment'i siliniz. Önce environment'i deactivate ediniz.
 ## Cevap:    conda deactivate
              conda env remove -n faruk
+
+
+Derya hocanin cevaplari
+###############################################
+# Virtual Environment Oluşturma
+###############################################
+
+
+
+# Görev 1: Kendi isminizde bir virtual environment oluşturunuz, oluşturma esnasında
+python 3 kurulumu yapınız.
+###############################################
+# conda create -n deryaenv python=3
+# conda env list (tüm env görebilirsiniz)
+###############################################
+
+# Görev 2: Oluşturduğunuz environment'ı aktif ediniz.
+###############################################
+#conda activate deryaenv
+###############################################
+
+# Görev 3: Yüklü paketleri listeleyiniz.
+###############################################
+# conda list
+###############################################
+
+# Görev 4: Environment içerisine Numpy'ın güncel versiyonunu ve Pandas'ın 1.2.1
+versiyonunu aynı anda indiriniz.
+###############################################
+# conda install numpy pandas=1.2.1
+###############################################
+
+# Görev 5: İndirilen Numpy'ın versiyonu nedir?
+###############################################
+# conda list
+###############################################
+
+# Görev 6: Pandas'ı upgrade ediniz. Yeni versiyon nedir?
+###############################################
+# conda upgrade pandas
+# conda list
+###############################################
+
+# Görev 7: Numpy'ı environment'tan siliniz.
+###############################################
+# conda remove numpy
+###############################################
+
+# Görev 8: Seaborn kütüphanesini ve matplotlib kütüphanesini aynı anda güncel
+versiyonları ile indiriniz.
+###############################################
+# conda install seaborn matplotlib
+###############################################
+
+# Görev 9: Virtual environment içindeki kütüphaneleri versiyon bilgisi ile beraber
+export ediniz ve yaml dosyasını inceleyiniz.
+###############################################
+# conda env export > enviroment.yaml
+###############################################
+
+# Görev 10: Oluşturduğunuz environment'i siliniz.
+###############################################
+# İpucu: Önce environment'i deactivate ediniz.
+# conda deactivate
+# conda env remove -n deryaenv
+
+
+
+###### ODEV 2 #########
+
+##### Python_Alistirmalar  #########
+
+### Görev 1: Verilen değerlerin veri yapılarını inceleyiniz.
+### Type() metodunu kullanınız.
+
+x = 8
+type(x)
+# int
+
+y = 3.2
+type(y)
+# float
+
+z = 8j + 18
+type(z)
+# complex
+
+a = "Hello World"
+type(a)
+# str
+
+b = True
+type(b)
+# bool
+
+c = 23 < 22
+type(c)
+# bool
+
+l = [1, 2, 3, 4]
+type(l)
+# list
+
+d = {"Name": "Jake",
+     "Age": 27,
+     "Adress": "Downtown"}
+type(d)
+# dict
+
+t = ("Machine Learning", "Data Science")
+type(t)
+# tuple
+
+s = {"Python", "Machine Learning", "Data Science"}
+type(s)
+# set
+
+##### Görev 2: Verilen string ifadenin tüm harflerini büyük harfe çeviriniz. Virgül ve nokta yerine space koyunuz,
+## kelime kelime ayırınız.
+
+## text = "The goal is to turn data into information, and information into insight."
+
+## Beklenen çıktı:
+## ['THE', 'GOAL', 'IS', 'TO', 'TURN','DATA', 'INTO', 'INFORMATION','AND','INFORMATION','INTO', 'INSIGHT']
+
+text = "The goal is to turn data into information, and information into insight."
+new_text = text.upper().replace(",", " ").replace(".", " ").split()
+print(new_text)
+
+
+##### Görev 3: Verilen listeye aşağıdaki adımları uygulayınız.
+
+lst = ["D", "A", "T", "A", "S", "C", "I", "E","N", "C", "E"]
+
+# Adım 1: Verilen listenin eleman sayısına bakınız.
+
+len(lst)
+# 11
+
+# Adım 2: Sıfırıncı ve onuncu indeksteki elemanları çağırınız.
+
+print(lst[0])
+print(lst[10])
+# 'D'
+# 'E'
+
+print(f"Sifirinci indeks: {lst[0]}, Onuncu indeks: {lst[10]}")
+## Sifirinci indeks: D, Onuncu indeks: E
+
+# Adım 3: Verilen liste üzerinden ["D", "A", "T", "A"] listesi oluşturunuz.
+
+new_list = lst[:4]
+print(f"olusturulan liste: {new_list}")
+## ["D", "A", "T", "A"]
+
+# Adım 4: Sekizinci indeksteki elemanı siliniz.
+
+lst.pop(8)
+print(lst)
+#['D', 'A', 'T', 'A', 'S', 'C', 'I', 'E', 'C', 'E']
+
+del lst[8]
+print(f"new_list: {lst}")
+## new_list: ['D', 'A', 'T', 'A', 'S', 'C', 'I', 'E', 'C', 'E']
+
+# Adım 5: Yeni bir eleman ekleyiniz.
+
+lst = ["D", "A", "T", "A", "S", "C", "I", "E","N", "C", "E"]
+lst.append("F")
+## ['D', 'A', 'T', 'A', 'S', 'C', 'I', 'E', 'N', 'C', 'E', 'F']
+
+# Adım 6: Sekizinci indekse "N" elemanını tekrar ekleyiniz.
+
+lst.insert(8, "N")
+print(lst)
+## ['D', 'A', 'T', 'A', 'S', 'C', 'I', 'E', 'N', 'N', 'C', 'E', 'F']
+
+
+##### Görev 4: Verilen sözlük yapısına aşağıdaki adımları uygulayınız.
+
+
+dict = {'Christian': ["America",18],
+        'Daisy': ["England",12],
+        'Antonio': ["Spain",22],
+        'Dante': ["Italy",25]}
+
+
+# Adım 1: Key değerlerine erişiniz.
+
+dict.keys()
+# dict_keys(['Christian', 'Daisy', 'Antonio', 'Dante'])
+
+# Adım 2: Value'lara erişiniz.
+
+dict.values()
+# dict_values([['America', 18], ['England', 12], ['Spain', 22], ['Italy', 25]])
+
+
+# Adım 3: Daisy key'ine ait 12 değerini 13 olarak güncelleyiniz.
+
+dict['Daisy'][1] = 13
+
+#{'Christian': ['America', 18],
+# 'Daisy': ['England', 13],
+# 'Antonio': ['Spain', 22],
+# 'Dante': ['Italy', 25]}
+
+
+# Adım 4: Key değeri Ahmet value değeri [Turkey,24] olan yeni bir değer ekleyiniz.
+
+dict['Ahmet'] = ['Turkey', 24]
+# {'Christian': ['America', 18],
+#  'Daisy': ['England', 13],
+#  'Antonio': ['Spain', 22],
+#  'Dante': ['Italy', 25],
+#  'Ahmet': ['Turkey', 24]}
+
+# Adım 5: Antonio'yu dictionary'den siliniz.
+
+del dict['Antonio']
+
+# {'Christian': ['America', 18],
+# 'Daisy': ['England', 13],
+# 'Dante': ['Italy', 25],
+# 'Ahmet': ['Turkey', 24]}
+
+
+## Görev 5: Argüman olarak bir liste alan, listenin içerisindeki
+# tek ve çift sayıları ayrı listelere atayan ve bu listeleri
+# return eden fonksiyon yazınız.
+
+l = [2, 13, 18, 93, 22]
+
+def sayilar(l):
+    cift_sayilar = []
+    tek_sayilar = []
+
+    for sayi in l:
+        if sayi % 2 == 0:
+            cift_sayilar.append(sayi)
+        else:
+            tek_sayilar.append(sayi)
+    return cift_sayilar, tek_sayilar
+
+print("Cift sayilar:", ciftler)
+print("Tek sayilar:", tekler)
+
+
+##### Görev 6: Aşağıda verilen listede mühendislik ve tıp fakülterinde dereceye giren öğrencilerin isimleri
+# bulunmaktadır. Sırasıyla ilk üç öğrenci mühendislik fakültesinin başarı sırasını temsil ederken son üç öğrenci de
+# tıp fakültesi öğrenci sırasına aittir. Enumarate kullanarak öğrenci derecelerini fakülte özelinde yazdırınız.
+
+ogrenciler = ["Ali", "Veli", "Ayse", "Talat", "Zeynep", "Ece"]
+
+
+for index, ogrenci in enumerate(ogrenciler[:3], 1):
+    print(f"Muhendislik Fakultesi {index}. ogrenci: {ogrenci}")
+for index, ogrenci in enumerate(ogrenciler[3:], 1):
+    print(f"Tip Fakultesi {index}. ogrenci: {ogrenci}")
+
+
+##### Görev 7: Aşağıda 3 adet liste verilmiştir. Listelerde sırası ile bir dersin kodu,
+# kredisi ve kontenjan bilgileri yer almaktadır. Zip kullanarak ders bilgilerini bastırınız.
+
+ders_kodu = ["CMP1005", "PSY1001", "HUK1005", "SEN2204"]
+kredi = [3,4,2,4]
+kontenjan = [30,75,150,25]
+
+for kod, kredi, kontenjan in zip(ders_kodu, kredi, kontenjan):
+    print(f"Kredisi {kredi} olan {kod} kodlu dersin kontenjani {kontenjan} kisidir.")
+
+# beklenen cikti:
+
+# Kredisi 3 olan CMP1005 kodlu dersin kontenjani 30 kisidir.
+# Kredisi 4 olan PSY1001 kodlu dersin kontenjani 75 kisidir.
+# Kredisi 2 olan HUK1005 kodlu dersin kontenjani 150 kisidir.
+# Kredisi 4 olan SEN2204 kodlu dersin kontenjani 25 kisidir.
+
+
+
+
+##### Görev 8: Aşağıda 2 adet set verilmiştir. Sizden istenilen eğer 1. küme 2. kümeyi kapsiyor ise
+# ortak elemanlarını eğer kapsamıyor ise 2. kümenin 1. kümeden farkını yazdıracak fonksiyonu tanımlamanız beklenmektedir.
+
+
+kume1 = set(["data", "python"])
+kume2 = set(["data", "function", "qcut", "lambda", "python", "miuul"])
+
+
+kume1.issuperset(kume2)
+# kapsamiyor
+
+kume1.intersection(kume2)
+kume2.difference(kume1)
+
+
+
+##### Odev 3 List Comprehension
+
+##### Görev 1: List Comprehension yapısı kullanarak car_crashes verisindeki numeric değişkenlerin isimlerini büyük
+#  harfe çeviriniz ve başına NUM ekleyiniz.
+
+import seaborn as sns
+df = sns.load_dataset("car_crashes")
+df.columns
+
+for col in df.columns:
+    print(col.upper())
+
+new_columns = ["NUM_" + col.upper() if df[col].dtypes in ['float', 'int'] else col.upper() for col in df.columns]
+
+print(new_columns)
+
+
+
+##### Görev 2: List Comprehension yapısı kullanarak car_crashes verisinde isminde "no" barındırmayan
+değişkenlerin isimlerinin sonuna "FLAG" yazınız
+
+
+import seaborn as sns
+df = sns.load_dataset("car_crashes")
+df.columns = [col.upper() for col in df.columns ]
+
+df.columns = [col + "FLAG_" if "NO" not in col else col for col in df.columns]
+
+
+
+##### Görev 3: List Comprehension yapısı kullanarak aşağıda verilen değişken isimlerinden FARKLI olan
+##    değişkenlerin isimlerini seçiniz ve yeni bir dataframe oluşturunuz.
+
+
+import seaborn as sns
+df = sns.load_dataset("car_crashes")
+df.columns
+
+og_list = ["abbrev", "no_previous"]
+new_cols = [col for col in df.columns if col not in og_list]
+new_df = df[new_cols]
+print(new_df)
+
+
+
+
+
+Önce verilen listeye göre list comprehension kullanarak new_cols adında yeni liste oluşturunuz.
+Sonra df[new_cols] ile bu değişkenleri seçerek yeni bir df oluşturunuz ve adını new_df olarak isimlendiriniz
+
+
+
+
+
+
+
+
+
+
+
 
